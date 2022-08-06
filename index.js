7cb927d628ba5d4d8e13d612dc58c1d6801e2dbe
Shiv Matta <shiv.matta@gmail.com>
2022-08-06 17:22:09 -0500

    commit tree dff0c7abc5ff357c456ad20776ce62eca84c2d80

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// Add your code here
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   };
+  
+//   const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json",
+//     },
+//     body: JSON.stringify(formData),
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (object) {
+//       console.log(object);
+//     })
+//     .catch(function (error) {
+//       alert("Bad things! Ragnarők!");
+//       console.log(error.message);
+//     });
+
+
+
+   
+    
+
+    function submitData(name, email){
+      return fetch("http://localhost:3000/users", {
+        method: 'POST', 
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        name, 
+        email
+      })
+    })
+    .then(response => response.json())
+    .then(response => {
+      document.body.innerHTML = `<p>${response.id}</p>`
+    })
+    .catch(error => {
+      document.body.innerHTML = `<p>${error.message}</p>`
+    })
+  }
+  
